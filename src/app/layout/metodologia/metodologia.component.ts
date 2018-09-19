1cb0a72992cfbb6b63cb4e722e54a8292ac7310c
gobs84 <gps9705@gmail.com>
2018-09-18 23:59:42 -0400

    adding metodologia page component

---
+++ src/app/layout/metodologia/metodologia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metodologia',
+  templateUrl: './metodologia.component.html',
+  styleUrls: ['./metodologia.component.scss']
+})
+export class MetodologiaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
