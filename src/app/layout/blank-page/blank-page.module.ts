30e9f16e9f66c4c01c5e9ea5a3ed33ab87258cd5
gobs84 <gps9705@gmail.com>
2018-10-01 16:00:48 -0400

    finishing date inputs in map

--- src/app/layout/blank-page/blank-page.module.ts
+++ src/app/layout/blank-page/blank-page.module.ts
@@ -8,12 +8,15 @@ import { AgmCoreModule } from '@agm/core';
 import { KmlLayerManager } from '@agm/core';
 import { GoogleMapsAPIWrapper  } from '@agm/core';
 import { PageHeaderModule } from '../../shared';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
     imports: [CommonModule, BlankPageRoutingModule,Ng2Charts,PageHeaderModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyB0JrpDZNJmHRd4YbrYOHOUdZsHL3QS-DU',
       libraries: ['geometry']
-    })],
+       
+    }),
+    FormsModule],
     providers: [GoogleMapsAPIWrapper],
     declarations: [BlankPageComponent]
 })
